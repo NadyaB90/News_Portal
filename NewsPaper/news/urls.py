526ed4de0e1655aedd4e2187cb92598083d4ae09
NadyaB90 <104001017+NadyaB90@users.noreply.github.com>
2022-08-04 23:23:24 +0500

    Update urls.py

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import PostsList, PostDetail, SearchList, PostCreateView
+from .views import PostsList, PostDetail, SearchList, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostsList.as_view()),
     path('<int:pk>', PostDetail.as_view()),
-    path('search/', SearchList.as_view()),
-    path('create/', PostCreateView.as_view(), name='post_create')
+    path('search/', SearchList.as_view(), name='post_search'),
+    path('create/', PostCreateView.as_view(), name='post_create'),
+    path('create/<int:pk>', PostUpdateView.as_view(), name='post_update'),
+    path('delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
 
 ]
 
